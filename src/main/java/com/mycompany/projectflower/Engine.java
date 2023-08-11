8d138c420bcb12e14a754c68f07218ff159844f4
ourie <ourie@ourienb23.ahasw.cz>
2023-08-11 08:27:25 +0200

    Preparation for new menu - Mainmenu

--- src/main/java/com/mycompany/projectflower/Engine.java
+++ src/main/java/com/mycompany/projectflower/Engine.java
@@ -5,62 +5,63 @@ import java.util.Scanner;
 
 public class Engine {
     //list menu item
-    
+    MainMenu mm;
     User usr;
-    Bank acc;
-    Flower f;
-    Shop sh;
-    Minigame mg;
     DatabaseHandler dh;
-    Browser br;
-    Curl cr;
-    Exit ex;
+    /*
+        Bank acc;
+        Flower f;
+        Shop sh;
+        Minigame mg;
+        Browser br;
+        Curl cr;
+        Exit ex;
+    */
     public void startGame(){
         usr = new User();
-        acc = new Bank();
-        f = new Flower();
-        sh = new Shop();
-        mg = new Minigame();
         dh = new DatabaseHandler();
-        br = new Browser();
-        cr = new Curl();
-        ex = new Exit();
-        
+        mm = new MainMenu();
+        /*
+            acc = new Bank();
+            f = new Flower();
+            sh = new Shop();
+            mg = new Minigame();
+            br = new Browser();
+            cr = new Curl();
+            ex = new Exit();
+        */
+
         System.out.println("Welcome!");
-        //dh.connectToDatabase();
-        //br.openBrowser();
-        cr.getPageData();
+        /*
+            dh.connectToDatabase();
+            br.openBrowser();
+            cr.getPageData();
+        */
         Scanner userinpt = new Scanner(System.in);
         
        
         
         while(true){
             System.out.println("What do you want to do?");
-            showmenu();
+            mm.showmenu();
             usr.setChoice(userinpt.nextInt());
             // z listu si vytáhnu userinput a zavolám execute
-            if(usr.getChoice()==1){
-                acc.accBalance();
-                
-            }
-            if(usr.getChoice()==4){
-                mg.start();
-            }
-            if(usr.getChoice()==5){
-                ex.Exitthegame();
-            }
+            /*
+                if(usr.getChoice()==1){
+                    acc.accBalance();
+
+                }
+                if(usr.getChoice()==4){
+                    mg.start();
+                }
+                if(usr.getChoice()==5){
+                    ex.Exitthegame();
+                }
+            */
             //Console.clearConsole();
             
             
         }
     }
-     public void showmenu(){
-        //index + jméno položky
-        //for cyklus přes lista zavloám menu
-        acc.name();
-        sh.menu();
-        f.menu();
-        mg.menu();
-        ex.menu();
-    }
+    
 }
--- src/main/java/com/mycompany/projectflower/MainMenu.java
+++ src/main/java/com/mycompany/projectflower/MainMenu.java
@@ -4,19 +4,40 @@ package com.mycompany.projectflower;
 import java.util.ArrayList;
 
 public class MainMenu implements MenuItem{
+    User usr;
+    Bank acc;
+    Flower f;
+    Shop sh;
+    Minigame mg;
+    DatabaseHandler dh;
+    Browser br;
+    Curl cr;
+    Exit ex;
     ArrayList<String> menu = new ArrayList<String>();
     String name = "MainMenu";
     public void execute(int user){
-        getItems();
+        System.out.println(menu.get(user));
     }
     public String name(){
         
        return name;
     }
     public ArrayList<String> getItems(){
-        menu.add("ads");
+        menu.add("ads"); // <-- jak dostamu do arrayListu hodnoty z ostatních Objektů prosím?
         
         
         return menu;
     } 
+     public void showmenu(){
+        //index + jméno položky
+        //for cyklus přes lista zavloám menu
+        for (String i : menu) {
+             System.out.println(i);
+        }
+        acc.name();
+        sh.menu();
+        f.menu();
+        mg.menu();
+        ex.menu();
+    }
 }
